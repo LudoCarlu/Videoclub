2c71634633193d44067ff4d4bf744c5b700380ff
Samuel <samuellaroche@live.ca>
2016-11-27 17:12:22 -0500

    Acquisition
    maintenant fonctionnelle

--- Videoclub/src/Acquisition.java
+++ Videoclub/src/Acquisition.java
@@ -14,7 +14,14 @@ public class Acquisition {
 	public void setListeAcquisition(ArrayList<Article> listeAcquisition) {
 		this.listeAcquisition = listeAcquisition;
 	}
-	
+	public void ajouterArticle(DescriptionArticle desc){
+		Article art = new Article();
+		art.setDescription(desc);
+		if (listeAcquisition == null){
+			listeAcquisition = new ArrayList<Article>();
+		}
+		listeAcquisition.add(art);
+	}
 	public void updatedb(){
 		
 		for (int i=0;i<listeAcquisition.size();i++){
@@ -23,22 +30,42 @@ public class Acquisition {
 		D.insertArticle(art.getDescription());
 		}
 	}
+	/* ces méthodes ont été ajouter au controler
 	public void ajouterFilm(String codeArticle, String titre, String genre, String description, float prixJournalier, 
 			float prixHebdomadaire, float prixVente, boolean estNouveau){
 		
-		Article art = null;
 		DescriptionArticle desc;
-		
-		
-		desc = new DescriptionArticle(codeArticle, description, prixVente, 
-				prixJournalier, titre, genre, estNouveau, prixHebdomadaire);
-		
+		desc = new DescriptionArticle();
+		desc.setCodeArticle(codeArticle);
+		desc.setTitre(titre);
+		desc.setGenre(genre);
+		desc.setDescription(description);
+		desc.setPrixJournalier(prixJournalier);
+		desc.setPrixHebdomadaire(prixHebdomadaire);
+		desc.setPrixVente(prixVente);
+		desc.setEstNouveau(estNouveau);
+	
+		Article art = new Article();
 		art.setDescription(desc);
-	}
-	public void ajouterAutre(){
 		
+		listeAcquisition.add(art);
 	}
 	
+	public void ajouterAutre(String codeArticle, String description, float prixVente){
+		
+		DescriptionArticle desc;
+		desc = new DescriptionArticle();
+		desc.setCodeArticle(codeArticle);
+		desc.setDescription(description);
+		desc.setTitre(description);
+		desc.setPrixVente(prixVente);
+		
+		Article art = new Article();
+		art.setDescription(desc);
+		
+		listeAcquisition.add(art);
+	}
+	*/
 }
 
 
