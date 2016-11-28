3e188681fd8a3727a93edbe1660b2ad08fccd6cc
Samuel <samuellaroche@live.ca>
2016-11-27 19:16:20 -0500

    Database : instance au répertoire utilisateur automatique

--- Videoclub/src/Database.java
+++ Videoclub/src/Database.java
@@ -32,17 +32,13 @@ public class Database {
 	}
 	
 	public static Database instanceDB() {
-		//Seulement enlevé le // du commentaire pour changer le path de la db
-		
+		//L'instance utilisera le bon répertoire peut importe l'ordinateur
 		String pathDB;
+		//retourne le répertoire du projet et ajoute l'emplacement du la db au path
+		pathDB = System.getProperty("user.dir")+"/database/testDB.db"; 
+		
 		//Maxime:
 		//pathDB = "/Users/maxime/Videoclub/Database/testDB.db";
-		
-		//Samuel:
-		pathDB = "/Users/samuel/Documents/Videoclub/Videoclub/database/testDB.db";
-	
-		//Ludo:
-		// pathDB: 
 		
 		if(instanceDB == null) {
 			instanceDB = new Database(pathDB);
@@ -271,5 +267,7 @@ public class Database {
 			e.printStackTrace();
 		}
 	}
+
 }
+
 
